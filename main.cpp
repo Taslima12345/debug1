5aa97f244dcf79645e20f8c30ca7e88e2dbd6e4c
Your Name <you@example.com>
2018-01-28 19:45:27 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x=5;
+    for(int i=10; i>= 0; i--)
+    {
+            int z = 50/(i-x);
+            cout << z << endl;
+    }
+
+    return 0;
+}
